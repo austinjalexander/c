e019975e398c659ad4fbad845c06a1959aeff24c
Austin J. Alexander <austinjalexander@gmail.com>
2013-12-03 13:47:05 -0500

    add some files

---
+++ prime_fact.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+main()
+{
+long num = 600851475143;
+
+int first_100_primes[100] = { 0 };
+int i;
+
+for (i = ; i < 100; i++)
+{
+if ()
+first_100_primes[i] = (i + 1) / 
+}
+
+
+/*
+//long half_of_num = (num / 2);
+//long i;
+
+//int factor;
+//long j;
+//int prime_truth;
+
+// count down backward from num
+for (i = (half_of_num); i > 1; i--)
+{
+// if i is a factor
+if ((num % i) == 0)
+{
+i = 1;
+}
+}
+*/
+/*
+// loop as many times as num
+for (i = 1; i <= num; i++)
+{
+// if the current i is a factor
+if (num % i == 0)
+{
+// check if i is a prime factor
+prime_truth = 1; 
+for (j = 2; j < i; j++)
+{
+// if any number j divides i, quit loop
+if ((i % j) == 0)
+{
+j = i;
+prime_truth = 0;
+printf("%d\n", i);
+}
+}
+// if the number is prime
+if (prime_truth == 1)
+{
+//printf("%d\n", i);
+}
+}
+} 
+*/
+
+}
