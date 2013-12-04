0e0de20c99ff468bbe60d2cf3e9fb37865fe1202
Austin J. Alexander <austinjalexander@gmail.com>
2013-12-04 16:41:21 -0500

    finished saving the princess

--- bot_saves_princess.c
+++ bot_saves_princess.c
@@ -4,7 +4,47 @@
 
 void displayPathtoPrincess(int n, char grid[n][n])
 {
-	
+	int m[2], p[2];
+	int i,j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			if (grid[i][j] == 'm')
+			{
+				m[0] = i;
+				m[1] = j;
+			}
+			if (grid[i][j] == 'p')
+			{
+				p[0] = i;
+				p[1] = j;
+			}
+		}
+	}
+
+	if (p[0] > m[0])
+	{
+		for (i = 1; i <= (p[0] - m[0]); i++)
+			printf("%s\n", "DOWN");
+	}
+	else if (m[1] > p[0])
+	{
+		for (i = 1; i <= (m[0] - p[0]); i++)
+			printf("%s\n", "UP");		
+	}
+
+	if (p[1] > m[1])
+	{
+		for (i = 1; i <= (p[1] - m[1]); i++)
+			printf("%s\n", "RIGHT");
+	}
+	else if (m[1] > p[1])
+	{
+		for (i = 1; i <= (m[1] - p[1]); i++)
+			printf("%s\n", "LEFT");		
+	}
 }
 
 int main(void)
