c526e6211ce38336ebf088f3ce3d34ba7b561e6c
Austin J. Alexander <austinjalexander@gmail.com>
2013-12-12 15:51:34 -0500

    abcheck finished

--- abcheck.c
+++ abcheck.c
@@ -1,33 +1,36 @@
 #include <stdio.h>
+#include <string.h>
 
-void ABCheck(char * str[])
+void ABCheck(char str[])
 {
-	int i;
+	int i, str_length, found = 0;
+	str_length = strlen(str);
 
-
-
-	for (i = 0; i < sizeof(*str); i++)
+	for (i = 0; i < str_length; i++)
 	{
 		if (str[i] == 'a')
 		{
 			if (str[i + 4] == 'b')
 			{
-				i = sizeof(*str);
-				rintf("%s", str);
+				found = 1;
+				i = str_length;
 			}
-				
 		}
 	}
 
-	p
+	if (found == 0)
+		printf("false\n");
+	else
+		printf("true\n");
 }
 
 int main(void)
 {
 
-	char input[] = "after badly";
+	char str[] = "Laura sobs";
+	//char str[] = "after badly";
 
-	ABCheck(input);
+	ABCheck(str);
 
 	return 0;
 }
