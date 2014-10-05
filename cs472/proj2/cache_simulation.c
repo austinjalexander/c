f990067fe433f759462eefa9d31095e4da037a1a
Austin J. Alexander <austinjalexander@gmail.com>
2014-10-05 00:46:49 -0400

    main memory initialized

--- cs472/proj2/cache_simulation.c
+++ cs472/proj2/cache_simulation.c
@@ -7,8 +7,8 @@
 #include <stdlib.h>
 #include <assert.h>
 
-// data length: 16, so 2^4, so 4 bits needed for slot index
 const short DATA_LENGTH = 16;
+
 struct CacheSlot {
   // slot index: (0, 1, 2, ..., F), so 4 bits
   short slot_index;
@@ -18,7 +18,8 @@ struct CacheSlot {
   short data[DATA_LENGTH];
 };
 
-struct CacheSlot *createCacheSlot(short slot_index, short valid_flag, short tag, short offset, short data) {
+struct CacheSlot *createCacheSlot(short slot_index, short valid_flag, short tag, 
+                                  short offset, short data) {
   struct CacheSlot *cache_slot = malloc(sizeof(struct CacheSlot));
   assert(cache_slot != NULL);
 
@@ -48,33 +49,50 @@ void printCacheSlot(struct CacheSlot *cache_slot) {
   
 } //printCacheSlot(cache_slot);
 
+void printCache(short CACHE_SIZE, struct CacheSlot *cache) {
+  printf(" SLOT  VALID  TAG     DATA\n");
+  for (int i = 0; i < CACHE_SIZE; i++) {
+    printCacheSlot(&cache[i]);
+  } 
+} //printCache(short CACHE_SIZE, struct CacheSlot *cache)
+
+void printMainMemory(short MEM_SIZE, short *main_memory) {
+  for (int i = 0; i < MEM_SIZE; i++) {
+    printf("[0x%x] = 0x%x | ", i, main_memory[i]);
+    if (main_memory[i] % 5 == 0) {
+      printf("\n");
+    }
+    if (main_memory[i] == 0xff) {
+      printf("*******<END 0xFF BLOCK>*******\n");
+    }
+  }
+
+} //printMainMemory(short MEM_SIZE, short *main_memory)
+
 int main()
 {
 
-  
+  /////// ******* CACHE ******* ///////
 
   const short CACHE_SIZE = 16;
+
+  // create cache (an array of CacheSlots)
   struct CacheSlot cache[CACHE_SIZE];
   for (int i = 0; i < CACHE_SIZE; i++) {
     struct CacheSlot *cache_slot = createCacheSlot(0 ,0, 0, 0, 0);
     cache[i] = *cache_slot;
   }
 
-  printf(" SLOT  VALID  TAG     DATA\n");
-  for (int i = 0; i < CACHE_SIZE; i++) {
-    printCacheSlot(&cache[i]);
-  }
+  // display initial cache values
+  //printCache(CACHE_SIZE, cache);
 
-  //short address_request = 0x7AE; // 0x000007AE
+  /////// ******* MAIN MEMORY ******* ///////
 
-  
-
-
-  // declare main memory
+  // create main memory
   const short MEM_SIZE = 2048;
   short main_memory[MEM_SIZE] = { 0 };
 
-  // initialize main memory
+  // give main memory initial values ([0x00] = 0, ..., [0x7FF] = 0xFF)
   short mem_value = 0x00;
   for (int i = 0; i < MEM_SIZE; i++) {
     if (mem_value < 0xFF) {
@@ -85,9 +103,14 @@ int main()
       main_memory[i] = mem_value;
       mem_value = 0x00; 
     }
-    //printf("main_memory[0x%x]: %d -> 0x%x\n", i, i, main_memory[i]);
   }
 
+  // display initial memory values
+  printMainMemory(MEM_SIZE, main_memory);
+
+
+
+  //short address_request = 0x7AE; // 0x000007AE
 
 
  
